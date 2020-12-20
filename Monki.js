9e677584e8c40e2ba355d7fe9981cd74efc560f3
Handreylel <76402178+Handreylel@users.noreply.github.com>
2020-12-20 14:05:09 -0300

    Código do macaco feito em js
    Nada a declarar

---
+++ Monki.js
@@ -0,0 +1,7 @@
+const retorneAMacaco = () => {
+  const monki = 'Macaco';
+  return monki;
+};
+
+const euVimVerOMacaco = retorneAMacaco();
+console.log(`Parabéns agora você é um ${euVimVerOMacaco}`);
